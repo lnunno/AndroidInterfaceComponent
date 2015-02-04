2f95a8bc6a7f142d12a8acceb8d554282fdc0836
Lucas <lucasnunno@gmail.com>
2015-02-03 23:49:50 -0700

    Got shake detection to work pretty well.

--- app/src/main/java/com/lnunno/interfacecomponent/MotionListView.java
+++ app/src/main/java/com/lnunno/interfacecomponent/MotionListView.java
@@ -11,12 +11,23 @@ import android.widget.ListView;
 
 /**
  * Created by Lucas on 2/3/2015.
+ *
+ * References:
+ *
+ * <ul>
+ *     <li>http://code.tutsplus.com/tutorials/using-the-accelerometer-on-android--mobile-22125</li>
+ * </ul>
  */
 public class MotionListView extends ListView implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor accelerometer;
     private Context context;
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 60;
+    private static final int UPDATE_INTERVAL_MILLIS = 100;
 
     public MotionListView(Context context){
         super(context);
@@ -47,18 +58,55 @@ public class MotionListView extends ListView implements SensorEventListener {
     * For SensorEventListener.
      */
     public void onSensorChanged(SensorEvent event){
-        Log.i("Sensor", "Sensor event triggered.");
+
+        Sensor sensor = event.sensor; // Get the sensor associated with this event.
+
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            registerAccelerometerEvent(event);
+        }
     }
 
     /*
     private methods.
      */
 
+    private void registerAccelerometerEvent(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        long curTimeMillis = System.currentTimeMillis();
+        long timeDelta = curTimeMillis - lastUpdate;
+
+        if (timeDelta > UPDATE_INTERVAL_MILLIS) {
+            // Do an update.
+            lastUpdate = curTimeMillis;
+
+            float speed = Math.abs(
+                    Math.abs(x) + Math.abs(y) + Math.abs(z)
+                            - (Math.abs(last_x) + Math.abs(last_y) + Math.abs(last_z))
+            ) / timeDelta * 1000;
+
+            Log.d("SPEED", "speed=" + speed);
+
+            if (speed > SHAKE_THRESHOLD) {
+                // The phone was shook!
+                Log.i("SHAKE", "Phone was shook!");
+            }
+
+            last_x = x;
+            last_y = y;
+            last_z = z;
+        } else {
+            return;
+        }
+    }
+
     private void initializeSensor(){
-        this.sensorManager = (SensorManager) context.getSystemService(context.SENSOR_SERVICE);
+        this.sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                accelerometer,
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
